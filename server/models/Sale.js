f6457354ff41c597dd056683d074eb0fbb25f0da
manna sharma <mannasharmaaralia@gmail.com>
2025-06-14 11:30:21 +0530

    sale Schema Added.

---
+++ server/models/Sale.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+const salesSchema = mongoose.Schema({
+    pet_food_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Inventory",
+        required :true
+    },
+
+   quantity_sold: {
+    type: Number,
+    required: true
+  },
+  total_revenue: {
+    type: Number,
+    required: true
+  },
+  sale_date: {
+    type: Date,
+    required: true
+  }
+}, { timestamps: true });
+
+const Sale = mongoose.model("Sale", salesSchema);
