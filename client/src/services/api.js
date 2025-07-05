c07bda113caed8fae0a8820d9fa38201d68e929b
manna sharma <mannasharmaaralia@gmail.com>
2025-07-06 03:09:06 +0530

    best build

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "https://pet-shop-inventory-management-system.onrender.com/";
+  process.env.REACT_APP_API_BASE_URL || "https://pet-shop-inventory-management-system.onrender.com";
 
 // Helper function to handle API responses
 const handleResponse = async (response) => {
