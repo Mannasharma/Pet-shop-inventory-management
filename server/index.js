6e2141aa1c2de38ce32f1c0f0aa8ea24064a21a1
manna sharma <mannasharmaaralia@gmail.com>
2025-07-06 01:14:58 +0530

    refactor: remove static file serving for separate frontend/backend hosting

--- server/index.js
+++ server/index.js
@@ -39,7 +39,8 @@ if (cluster.isMaster) {
   // Middleware
   app.use(
     cors({
-      origin: process.env.NODE_ENV === "production" ? false : "http://localhost:3000",
+      origin:
+        process.env.NODE_ENV === "production" ? false : "http://localhost:3000",
       credentials: true,
     })
   );
@@ -53,14 +54,6 @@ if (cluster.isMaster) {
   app.use("/inventory", ristrictTo(["NORMAL", "ADMIN"]), inventoryRouter);
   app.use("/sales", ristrictTo(["NORMAL", "ADMIN"]), saleRouter);
   app.use("/report", ristrictTo(["ADMIN"]), reportRouter);
-
-  // Serve static files from the React app
-  app.use(express.static(path.join(__dirname, "build")));
-  
-  // Handle React routing, return all requests to React app
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
 
   // Start server
   app.listen(port, () => {
